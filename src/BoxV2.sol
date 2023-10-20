92c7fc68110f1457f0b5b8f3fc482b57aa693979
Sardius <0xsardius@gmail.com>
2023-10-20 16:32:36 -0400

    adding openzeppelin proxies

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.18;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract BoxV1 is UUPSUpgradeable {
     uint256 internal number;
@@ -18,11 +19,4 @@ contract BoxV1 is UUPSUpgradeable {
     function version() external pure returns (uint256) {
         return 2;
     }
-
-    function _authorizeUpgrade(address newImplementation)
-        internal
-        override
-    {
-        
-    }  
 }
